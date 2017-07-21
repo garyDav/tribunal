f49537d5e573b98de28108ad59ab73105c272aea
Gary David Guzmán Muñoz <gary.2810.dav@gmail.com>
2017-07-21 12:51:31 -0400

    modificado galeria felicitaciones

--- rest/v1/models/connect.php
+++ rest/v1/models/connect.php
@@ -6,7 +6,6 @@ function getConex() {
 		$us = "root";
 		//$pwd = "";
 		$pwd = "garydavid";
-		$pwd = "";
 		$conex = new PDO("mysql:host=localhost;dbname=tribunal;charset=utf8",$us,$pwd,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 		$conex->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	}catch(PDOException $e) {
